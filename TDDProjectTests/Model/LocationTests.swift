a11b2da0e4ffe1245e809097bf52bcb3d7e2e0a5
RyoYamada <newfield1001@gmail.com>
2018-11-28 00:29:29 +0900

    Add LocationTests.swift

---
+++ TDDProjectTests/Model/LocationTests.swift
@@ -0,0 +1,22 @@
+//
+//  LocationTests.swift
+//  TDDProjectTests
+//
+//  Created by yamada.ryo on 2018/11/28.
+//  Copyright © 2018年 yamada.ryo. All rights reserved.
+//
+
+import XCTest
+
+class LocationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+}
